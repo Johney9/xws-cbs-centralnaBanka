6f27aa5737a500fead029b96e0c47aa13bc4bccc
Nikola Mihalek <nikola.mihalek@gmail.com>
2015-03-31 00:44:58 +0200

    fixing wsdl, generating new files

--- gen/rs/ac/uns/ftn/xws/cbs/mt103/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.ftn.uns.ac.rs/xws/cbs/mt103", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package rs.ac.uns.ftn.xws.cbs.mt103;
